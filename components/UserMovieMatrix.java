e81913256f096726b9f6b09e1aec1cf1fec8bfba
tanuanje <tanuanje@UTORONTO.CA>
2018-08-05 17:18:58 +0000

    changed implementation to generic array type

--- components/UserMovieMatrix.java
+++ components/UserMovieMatrix.java
@@ -1,10 +1,5 @@
 package components;
 
 public class UserMovieMatrix<E> extends Matrix<E> implements MatrixInterface<E>{
-
-  @Override
-  public void populateMatrix(int row, int col, E input) {
-    content[row][col] = input;
-  }
   
 }
