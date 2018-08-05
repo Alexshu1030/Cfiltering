2254860c3559ff2c99f9d6be2e2f074d2ce678f1
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@6c2ab6b3-a9c7-4b03-9a9e-24852ae25e8f>
2018-08-05 17:44:05 +0000

    added attributes numOfRows and numOfCols

--- Assignment3/src/components/Matrix.java
+++ Assignment3/src/components/Matrix.java
@@ -3,13 +3,17 @@ package components;
 public class Matrix<E> implements MatrixInterface<E>{
 
   protected E[][] content;
-
+  protected int numOfRows;
+  protected int numOfCols;
+  
   public Matrix() {
 	  this.content = (E[][]) new Object[0][0];
   }
 
   public Matrix(int row, int col) {
     this.content = (E[][]) new Object[row][col];
+    this.numOfRows = row;
+    this.numOfCols = col;
   }
 
   public void populateMatrix(int row, int col, E input) {
