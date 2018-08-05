18f414aeb8b868e014cf34990d0372d2f93b2d82
tanuanje <tanuanje@UTORONTO.CA>
2018-08-05 17:18:46 +0000

    changed implementation to generic array type

--- components/MatrixInterface.java
+++ components/MatrixInterface.java
@@ -2,9 +2,8 @@ package components;
 
 public interface MatrixInterface<E> {
   
-  public void construct(int row, int col);
   public void populateMatrix(int row, int col, E input);
-  public int get(int row, int col);
+  public E get(int row, int col);
   public String toString();
 
 }
