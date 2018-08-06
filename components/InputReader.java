8487ee7e52d3695b8f79e439d8ebff8ce19b88e9
tanuanje <tanuanje@UTORONTO.CA>
2018-08-06 18:01:13 +0000

    format

--- components/InputReader.java
+++ components/InputReader.java
@@ -1,3 +1,18 @@
+// **********************************************************
+// Assignment3:
+// UTORID: tanuanje
+// UT Student #: 1003939982
+// Author: Jeremy Tanuan
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences. In this semester
+// we will select any three of your assignments from total of 5 and run it
+// for plagiarism check.
+// *********************************************************
 package components;
 
 import java.io.BufferedReader;
@@ -8,65 +23,63 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class InputReader {
-	public static <E> String read() {
-		String output = "";
-		try {
+  public static <E> String read() {
+    String output = "";
+    try {
 
-			// open file to read
-			String fileName;
-			Scanner in = new Scanner(System.in);
-			System.out.println("Enter the name of input file? ");
-			fileName = in.nextLine();
-			FileInputStream fStream = new FileInputStream(fileName);
-			BufferedReader br =
-					new BufferedReader(new InputStreamReader(fStream));
+      // open file to read
+      String fileName;
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter the name of input file? ");
+      fileName = in.nextLine();
+      FileInputStream fStream = new FileInputStream(fileName);
+      BufferedReader br = new BufferedReader(new InputStreamReader(fStream));
 
-			// Read dimensions: number of users and number of movies
-			int numberOfUsers = Integer.parseInt(br.readLine());
-			int numberOfMovies = Integer.parseInt(br.readLine());
+      // Read dimensions: number of users and number of movies
+      int numberOfUsers = Integer.parseInt(br.readLine());
+      int numberOfMovies = Integer.parseInt(br.readLine());
 
-			/*
-			 * create a new Cfiltering object that contains: a) 2d matrix
-			 * i.e.userMovieMatrix (#users*#movies) b) 2d matrix i.e.
-			 * userUserMatrix (#users*#users)
-			 */
-			Cfiltering cfObject =
-					new Cfiltering(numberOfUsers, numberOfMovies);
+      /*
+       * create a new Cfiltering object that contains: a) 2d matrix
+       * i.e.userMovieMatrix (#users*#movies) b) 2d matrix i.e. userUserMatrix
+       * (#users*#users)
+       */
+      Cfiltering cfObject = new Cfiltering(numberOfUsers, numberOfMovies);
 
-			// this is a blank line being read
-			br.readLine();
+      // this is a blank line being read
+      br.readLine();
 
-			// read each line of movie ratings and populate the
-			// userMovieMatrix
-			String row;
-			int rowNumber = 0;
+      // read each line of movie ratings and populate the
+      // userMovieMatrix
+      String row;
+      int rowNumber = 0;
 
-			while ((row = br.readLine()) != null) {
-				// allRatings is a list of all String numbers on one row
-				String allRatings[] = row.split(" ");
-				int columnNumber = 0;
-				for (String singleRating : allRatings) {  
-					// make the String number into an integer
-					int currentRating = Integer.parseInt(singleRating);
-					// populate userMovieMatrix
-					cfObject.populateUserMovieMatrix(rowNumber, columnNumber,
-							currentRating);
-					columnNumber++;
-				}
-				rowNumber++;  
-			}
-			// close the file
-			fStream.close();
+      while ((row = br.readLine()) != null) {
+        // allRatings is a list of all String numbers on one row
+        String allRatings[] = row.split(" ");
+        int columnNumber = 0;
+        for (String singleRating : allRatings) {
+          // make the String number into an integer
+          int currentRating = Integer.parseInt(singleRating);
+          // populate userMovieMatrix
+          cfObject.populateUserMovieMatrix(rowNumber, columnNumber,
+              currentRating);
+          columnNumber++;
+        }
+        rowNumber++;
+      }
+      // close the file
+      fStream.close();
 
-			output = cfObject.run();
+      output = cfObject.run();
 
-		} catch (FileNotFoundException e) {
-			System.err.println("Do you have the input file in the root"
-					+ " folder of your project?");
-			System.err.print(e.getMessage());
-		} catch (IOException e) {
-			System.err.print(e.getMessage());
-		}
-		return output;
-	}
+    } catch (FileNotFoundException e) {
+      System.err.println("Do you have the input file in the root"
+          + " folder of your project?");
+      System.err.print(e.getMessage());
+    } catch (IOException e) {
+      System.err.print(e.getMessage());
+    }
+    return output;
+  }
 }
