1cd64bbef252dd0c2f6fca5a50ea095341a48260
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@6c2ab6b3-a9c7-4b03-9a9e-24852ae25e8f>
2018-08-06 18:01:05 +0000

    format

--- Assignment3/src/components/Matrix.java
+++ Assignment3/src/components/Matrix.java
@@ -1,13 +1,28 @@
+// **********************************************************
+// Assignment3:
+// UTORID: tanuanje
+// UT Student #: 1003939982
+// Author: Jeremy Tanuan
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences. In this semester
+// we will select any three of your assignments from total of 5 and run it
+// for plagiarism check.
+// *********************************************************
 package components;
 
-public class Matrix<E> implements MatrixInterface<E>{
+public class Matrix<E> implements MatrixInterface<E> {
 
   protected E[][] content;
   protected int numOfRows;
   protected int numOfCols;
-  
+
   public Matrix() {
-	  this.content = (E[][]) new Object[0][0];
+    this.content = (E[][]) new Object[0][0];
   }
 
   public Matrix(int row, int col) {
@@ -20,10 +35,10 @@ public class Matrix<E> implements MatrixInterface<E>{
     content[row][col] = input;
   }
 
-  public E get(int row, int col) { 
+  public E get(int row, int col) {
     return content[row][col];
   }
-  
+
   @Override
   public String toString() {
     return "";
