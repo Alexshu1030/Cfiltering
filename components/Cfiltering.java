97eada74d86e6bed585b87160f75db1ca33dc439
tanuanje <tanuanje@UTORONTO.CA>
2018-08-06 00:52:26 +0000

    implemented printUserUser and mostSimilarScore

--- components/Cfiltering.java
+++ components/Cfiltering.java
@@ -18,156 +18,223 @@ package components;
 import java.text.DecimalFormat;
 
 public class Cfiltering<E> {
-  // this is a 2d matrix i.e. user*movie
-  private Matrix<E> userMovieMatrix;
-  // this is a 2d matrix i.e. user*movie
-  private Matrix<E> userUserMatrix;
+	// this is a 2d matrix i.e. user*movie
+	private Matrix<E> userMovieMatrix;
+	// this is a 2d matrix i.e. user*movie
+	private Matrix<E> userUserMatrix;
 
-  /**
-   * Default Constructor.
-   */
-  public Cfiltering() {
-  }
+	/**
+	 * Default Constructor.
+	 */
+	public Cfiltering() {
+	}
 
-  /*
-   * TODO:COMPLETE THIS I.E. APPROPRIATELY CREATE THE userMovieMatrix AND
-   * userUserMatrix WITH CORRECT DIMENSIONS.
-   */
-  /**
-   * Constructs an object which contains two 2d matrices, one of size
-   * users*movies which will store integer movie ratings and one of size
-   * users*users which will store float similarity scores between pairs of
-   * users.
-   * 
-   * @param numberOfUsers Determines size of matrix variables.
-   * @param numberOfMovies Determines size of matrix variables.
-   */
-  public Cfiltering(int numberOfUsers, int numberOfMovies) {
-	  userMovieMatrix = new Matrix<E>(numberOfUsers, numberOfMovies);
-	  userUserMatrix = new Matrix<E>(numberOfUsers, numberOfMovies);
-  }
+	/*
+	 * TODO:COMPLETE THIS I.E. APPROPRIATELY CREATE THE userMovieMatrix AND
+	 * userUserMatrix WITH CORRECT DIMENSIONS.
+	 */
+	/**
+	 * Constructs an object which contains two 2d matrices, one of size
+	 * users*movies which will store integer movie ratings and one of size
+	 * users*users which will store float similarity scores between pairs of
+	 * users.
+	 * 
+	 * @param numberOfUsers Determines size of matrix variables.
+	 * @param numberOfMovies Determines size of matrix variables.
+	 */
+	public Cfiltering(int numberOfUsers, int numberOfMovies) {
+		userMovieMatrix = new Matrix<E>(numberOfUsers, numberOfMovies);
+		userUserMatrix = new Matrix<E>(numberOfUsers, numberOfMovies);
+	}
 
-  /**
-   * The purpose of this method is to populate the UserMovieMatrix. As input
-   * parameters it takes in a rowNumber, columnNumber and a rating value. The
-   * rating value is then inserted in the UserMovieMatrix at the specified
-   * rowNumber and the columnNumber.
-   * 
-   * @param rowNumber The row number of the userMovieMatrix.
-   * @param columnNumber The column number of the userMovieMatrix.
-   * @param ratingValue The ratingValue to be inserted in the userMovieMatrix
-   */
-  public void populateUserMovieMatrix(int row, int col,
-      E input) {
-    userMovieMatrix.populateMatrix(row, col, input);
-  }
+	/**
+	 * The purpose of this method is to populate the UserMovieMatrix. As input
+	 * parameters it takes in a rowNumber, columnNumber and a rating value. The
+	 * rating value is then inserted in the UserMovieMatrix at the specified
+	 * rowNumber and the columnNumber.
+	 * 
+	 * @param rowNumber The row number of the userMovieMatrix.
+	 * @param columnNumber The column number of the userMovieMatrix.
+	 * @param ratingValue The ratingValue to be inserted in the userMovieMatrix
+	 */
+	public void populateUserMovieMatrix(int row, int col,
+			E input) {
+		userMovieMatrix.populateMatrix(row, col, input);
+	}
 
-  /*
-   * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
-   * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC. Add/remove
-   * 
-   * @param AND
-   * 
-   * @return as required below.
-   */
-  /**
-   * Determines how similar each pair of users is based on their ratings. This
-   * similarity value is represented with with a float value between 0 and 1,
-   * where 1 is perfect/identical similarity. Stores these values in the
-   * userUserMatrix.
-   * 
-   * @param COMPLETE THIS IF NEEDED
-   * @param COMPLETE THIS IF NEEDED
-   * @return COMPLETE THIS IF NEEDED
-   */
-  public void calculateSimilarityScore() {
-	// instantiate decimal accuracy of 4 decimal places
-	    DecimalFormat df = new DecimalFormat("0.0000");
-	    float similarityScore = 0;
-	    int i;
-	    int numOfUsers = userMovieMatrix.numOfRows;
-	    int numOfMovies = userMovieMatrix.numOfCols;
-	    // compares each user against one another to generate similarity
-	    // score
-	    for (i = 0; i < numOfUsers; i++) {
-	      for (int k = 1 + i; k < numOfUsers; k++) {
-	        // this is the total of all ratings given by 
-	        // (user1's rating of movie1-user2's rating of movie1)^2 + 
-	        // (user1's rating of movie2-user2's rating of movie2)^2 +
-	        // and so on...
-	        int summation = 0;
-	        // get rating of each movie of user1 and user2 and add
-	        // to summation
-	        for (int j = 0; j <= numOfMovies - 1; j++) {
-	          E rating1 = userMovieMatrix.get(i, j);
-	          E rating2 = userMovieMatrix.get(k, j);
-	          try {
-	        	  summation += (int) Math.pow((int)rating1 - (int)rating2, 2);
-	          } catch (InvalidRatingEntry e) {
-	        	  
-	          }
-	        }
-	        // perform formula for similarity score
-	        similarityScore = (float) (1 / (1 + Math.sqrt(summation)));
-	        // format score to hold 4 decimal places including trailing 0's
-	        E result = (E) df.format(similarityScore);
-	        // fill user-user matrix with score
-	        userUserMatrix.populateMatrix(i, k, result);
-	        userUserMatrix.populateMatrix(k, i, result);
-	      }
-	      // fill user-user matrix's main diagonal with 1.0000
-	      userUserMatrix.populateMatrix(i, i, (E) "1.0000");
-	    }
-  }
+	/*
+	 * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
+	 * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC. Add/remove
+	 * 
+	 * @param AND
+	 * 
+	 * @return as required below.
+	 */
+	/**
+	 * Determines how similar each pair of users is based on their ratings. This
+	 * similarity value is represented with with a float value between 0 and 1,
+	 * where 1 is perfect/identical similarity. Stores these values in the
+	 * userUserMatrix.
+	 * 
+	 * @param COMPLETE THIS IF NEEDED
+	 * @param COMPLETE THIS IF NEEDED
+	 * @return COMPLETE THIS IF NEEDED
+	 */
+	public void calculateSimilarityScore() {
+		// instantiate decimal accuracy of 4 decimal places
+		DecimalFormat df = new DecimalFormat("0.0000");
+		float similarityScore = 0;
+		int i;
+		int numOfUsers = userMovieMatrix.numOfRows;
+		int numOfMovies = userMovieMatrix.numOfCols;
+		// compares each user against one another to generate similarity
+		// score
+		for (i = 0; i < numOfUsers; i++) {
+			for (int k = 1 + i; k < numOfUsers; k++) {
+				// this is the total of all ratings given by 
+				// (user1's rating of movie1-user2's rating of movie1)^2 + 
+				// (user1's rating of movie2-user2's rating of movie2)^2 +
+				// and so on...
+				int summation = 0;
+				// get rating of each movie of user1 and user2 and add
+				// to summation
+				for (int j = 0; j <= numOfMovies - 1; j++) {
+					E rating1 = userMovieMatrix.get(i, j);
+					E rating2 = userMovieMatrix.get(k, j);
+					try {
+						summation += (int) Math.pow((int)rating1 - (int)rating2, 2);
+					} catch (InvalidRatingEntry e) {
+						//TODO invalidRatingEntry
+					}
+				}
+				// perform formula for similarity score
+				similarityScore = (float) (1 / (1 + Math.sqrt(summation)));
+				// format score to hold 4 decimal places including trailing 0's
+				E result = (E) df.format(similarityScore);
+				// fill user-user matrix with score
+				userUserMatrix.populateMatrix(i, k, result);
+				userUserMatrix.populateMatrix(k, i, result);
+			}
+			// fill user-user matrix's main diagonal with 1.0000
+			userUserMatrix.populateMatrix(i, i, (E) "1.0000");
+		}
+	}
 
-  /*
-   * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
-   * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC
-   */
-  /**
-   * Prints out the similarity scores of the userUserMatrix, with each row and
-   * column representing each/single user and the cell position (i,j)
-   * representing the similarity score between user i and user j.
-   * 
-   * @param COMPLETE THIS IF NEEDED
-   * @param COMPLETE THIS IF NEEDED
-   * @return COMPLETE THIS IF NEEDED
-   */
+	/*
+	 * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
+	 * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC
+	 */
+	/**
+	 * Prints out the similarity scores of the userUserMatrix, with each row and
+	 * column representing each/single user and the cell position (i,j)
+	 * representing the similarity score between user i and user j.
+	 * 
+	 * @param COMPLETE THIS IF NEEDED
+	 * @param COMPLETE THIS IF NEEDED
+	 * @return COMPLETE THIS IF NEEDED
+	 */
 
-  public void printUserUserMatrix() {
+	public void printUserUserMatrix() {
+		int numOfUsers = userUserMatrix.numOfRows;
+		// create two break lines to separate each section of output
+		System.out.println("\n");
+		System.out.println("userUserMatrix is: ");
+		// print each user-user similarity score
+		for (int i = 0; i < numOfUsers; i++) {
+			System.out.print("[");
+			for (int j = 0; j < numOfUsers; j++) {
+				System.out.print(userUserMatrix.get(i, j));
+				if (j < numOfUsers - 1) {
+					System.out.print(", ");
+				}
+			}
+			System.out.println("]");
+		}
+	}
 
-  }
+	/*
+	 * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
+	 * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC
+	 */
+	/**
+	 * This function finds and prints the most similar pair of users in the
+	 * userUserMatrix.
+	 * 
+	 * @param COMPLETE THIS IF NEEDED
+	 * @param COMPLETE THIS IF NEEDED
+	 * @return COMPLETE THIS IF NEEDED
+	 */
 
-  /*
-   * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
-   * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC
-   */
-  /**
-   * This function finds and prints the most similar pair of users in the
-   * userUserMatrix.
-   * 
-   * @param COMPLETE THIS IF NEEDED
-   * @param COMPLETE THIS IF NEEDED
-   * @return COMPLETE THIS IF NEEDED
-   */
+	public void findAndprintMostSimilarPairOfUsers() {
+		int numOfUsers = userUserMatrix.numOfRows;
+		// create two break lines to separate each section of output
+		System.out.println("\n");
+		System.out.println("The most similar pairs of users from above"
+				+ " userUserMatrix are: ");
+		float highestScore = 0;
+		int j = 0;
+		// instantiate list of tuples holding the closest scored pairs
+		// [amount of closest pairs][0: user1 in pair, 1: user2 in pair]
+		int closestPair[][] = new int[numOfUsers * (numOfUsers - 1)][2];
+		// find user pairs with most similar score
+		for (int i = 0; i < numOfUsers; i++) {
+			for (int k = 1 + i; k < numOfUsers; k++) {
+				// add current pair to list of closestPair if it has the same
+				// score as the highest known score
+				try {
+					float curScore = Float.parseFloat((String) userUserMatrix.get(i, k));
+					if (curScore == highestScore) {
+						closestPair[j][0] = i;
+						closestPair[j][1] = k;
+						j++;
+						// if new highest score is found, erase previous entries to
+						// closestPair, add current pair to closestPair, and
+						// assert new highest score
+					} else if (curScore > highestScore) {
+						j = 0;
+						// erase any previous entries to closestPair
+						for (int l = 0; l < numOfUsers * (numOfUsers - 1); l++) {
+							closestPair[l][0] = 0;
+							closestPair[l][1] = 0;
+						}
+						// add current pair to list of closestPair
+						closestPair[j][0] = i;
+						closestPair[j][1] = k;
+						// assert new highest score
+						highestScore = Float.parseFloat((String) userUserMatrix.get(i, k));
+						j++;
+					}
+				} catch (InvalidRatingEntry e) {
+					//TODO invalidRatingEntry
+				}
+			}
+		}
+		// print user pairs with most similar score
+		for (int l = 0; l <= j; l++) {
+			if (!(closestPair[l][0] == 0 && closestPair[l][1] == 0)) {
+				System.out.println("User" + (closestPair[l][0] + 1)
+						+ " and User" + (closestPair[l][1] + 1));
+			}
+		}
+		// change score to decimal accuracy of 4 places and print
+		DecimalFormat df = new DecimalFormat("0.0000");
+		String result = df.format(highestScore);
+		System.out.println("with similarity score of " + result);
+	}
 
-  public void findAndprintMostSimilarPairOfUsers() {
+	/*
+	 * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
+	 * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC
+	 */
+	/**
+	 * This function finds and prints the most dissimilar pair of users in the
+	 * userUserMatrix.
+	 * 
+	 * @param COMPLETE THIS IF NEEDED
+	 * @param COMPLETE THIS IF NEEDED
+	 * @return COMPLETE THIS IF NEEDED
+	 */
+	public void findAndprintMostDissimilarPairOfUsers() {
 
-  }
-
-  /*
-   * TODO:COMPLETE THIS YOU ARE FREE TO CHANGE THE FUNCTION SIGNATURE BUT DO NOT
-   * CHANGE THE FUNCTION NAME AND DO NOT MAKE THIS FUNCTION STATIC
-   */
-  /**
-   * This function finds and prints the most dissimilar pair of users in the
-   * userUserMatrix.
-   * 
-   * @param COMPLETE THIS IF NEEDED
-   * @param COMPLETE THIS IF NEEDED
-   * @return COMPLETE THIS IF NEEDED
-   */
-  public void findAndprintMostDissimilarPairOfUsers() {
-
-  }
+	}
 }
