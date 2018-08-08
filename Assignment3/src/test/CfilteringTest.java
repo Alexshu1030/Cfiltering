6866a907bdc91dcc3aa2ff37f0cfb695ee6cd34a
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@6c2ab6b3-a9c7-4b03-9a9e-24852ae25e8f>
2018-08-08 02:06:54 +0000

    fixed tests

--- Assignment3/src/test/CfilteringTest.java
+++ Assignment3/src/test/CfilteringTest.java
@@ -19,40 +19,7 @@ public class CfilteringTest<E> {
 
   @Test
   public void runTest() {
-    String expected = "\n" + 
-        "\n" + 
-        "userUserMatrix is: \n" + 
-        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
-        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
-        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
-        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
-        "\n" + 
-        "\n" + 
-        "The most dissimilar pairs of users from above userUserMatrix are: \n" + 
-        "User1 and User2\n" + 
-        "User1 and User3\n" + 
-        "User1 and User4\n" + 
-        "User2 and User3\n" + 
-        "User2 and User4\n" + 
-        "User3 and User4\n" + 
-        "with similarity score of 1.0000\n" + 
-        "\n" + 
-        "\n" + 
-        "The most dissimilar pairs of users from above userUserMatrix are: \n" + 
-        "User1 and User2\n" + 
-        "User1 and User3\n" + 
-        "User1 and User4\n" + 
-        "User2 and User3\n" + 
-        "User2 and User4\n" + 
-        "User3 and User4\n" + 
-        "with similarity score of 1.0000\n" + 
-        "";
-    Cfiltering cfObject = new Cfiltering(UMmatrix, UUmatrix);
-    String actual = cfObject.run();
-    assertEquals(expected, actual);
-  }
-  /*
-   * "\n" + 
+    String expected =  "\n" + 
         "\n" + 
         "userUserMatrix is: \n" + 
         "[1.0000, 0.1380, 0.2171, 0.1285]\n" + 
@@ -70,6 +37,68 @@ public class CfilteringTest<E> {
         "User3 and User4\n" + 
         "with similarity score of 0.1250\n" + 
         "";
+    Cfiltering cfObject = new Cfiltering(UMmatrix, UUmatrix);
+    String actual = cfObject.run();
+    assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void populateTest() {
+    String expected =  "\n" + 
+        "\n" + 
+        "userUserMatrix is: \n" + 
+        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
+        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
+        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
+        "[1.0000, 1.0000, 1.0000, 1.0000]\n" + 
+        "\n" + 
+        "\n" + 
+        "The most dissimilar pairs of users from above userUserMatrix are: \n" + 
+        "User1 and User2\n" +
+        "User1 and User3\n" +
+        "User1 and User4\n" +
+        "User2 and User3\n" +
+        "User2 and User4\n" +
+        "User3 and User4\n" +
+        "with similarity score of 1.0000\n" + 
+        "\n" + 
+        "\n" + 
+        "The most dissimilar pairs of users from above userUserMatrix are: \n" + 
+        "User1 and User2\n" +
+        "User1 and User3\n" +
+        "User1 and User4\n" +
+        "User2 and User3\n" +
+        "User2 and User4\n" +
+        "User3 and User4\n" + 
+        "with similarity score of 1.0000\n" + 
+        "";
+    Cfiltering cfObject = new Cfiltering(UMmatrix, UUmatrix);
+    cfObject.populateUserMovieMatrix(0, 0, 1);
+    cfObject.populateUserMovieMatrix(0, 1, 1);
+    cfObject.populateUserMovieMatrix(0, 2, 1);
+    cfObject.populateUserMovieMatrix(0, 3, 1);
+    cfObject.populateUserMovieMatrix(0, 4, 1);
+    cfObject.populateUserMovieMatrix(1, 0, 1);
+    cfObject.populateUserMovieMatrix(1, 1, 1);
+    cfObject.populateUserMovieMatrix(1, 2, 1);
+    cfObject.populateUserMovieMatrix(1, 3, 1);
+    cfObject.populateUserMovieMatrix(1, 4, 1);
+    cfObject.populateUserMovieMatrix(2, 0, 1);
+    cfObject.populateUserMovieMatrix(2, 1, 1);
+    cfObject.populateUserMovieMatrix(2, 2, 1);
+    cfObject.populateUserMovieMatrix(2, 3, 1);
+    cfObject.populateUserMovieMatrix(2, 4, 1);
+    cfObject.populateUserMovieMatrix(3, 0, 1);
+    cfObject.populateUserMovieMatrix(3, 1, 1);
+    cfObject.populateUserMovieMatrix(3, 2, 1);
+    cfObject.populateUserMovieMatrix(3, 3, 1);
+    cfObject.populateUserMovieMatrix(3, 4, 1);
+
+    String actual = cfObject.run();
+    assertEquals(expected, actual);
+  }
+  /*
+   *
    */
 
 }
