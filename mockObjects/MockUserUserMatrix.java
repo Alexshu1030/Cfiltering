399cdb4ff8d650ed13c71a7b57aa5a3dd86f0f0c
tanuanje <tanuanje@UTORONTO.CA>
2018-08-08 02:06:29 +0000

    added implementation

--- mockObjects/MockUserUserMatrix.java
+++ mockObjects/MockUserUserMatrix.java
@@ -2,15 +2,24 @@ package mockObjects;
 
 public class MockUserUserMatrix<E> extends MockMatrix<E>{
 
-  public MockUserUserMatrix(int row) {
-    content = (E[][]) new Object[row][row];
+  public MockUserUserMatrix() {
+    this.content = (E[][]) new Object[0][0];
   }
-  
-  @Override
-  public E get(int row, int col) throws ArrayIndexOutOfBoundsException {
-    if (row >= 0 && row < 9 && col >= 0 && col < 9) {
-      return (E)(Object) "1.0000";
+
+  public MockUserUserMatrix(int row) throws ArrayIndexOutOfBoundsException{
+    if (row > 0) {
+      this.content = (E[][]) new Object[row][row];
+      this.numOfRows = row;
+      this.numOfCols = row;
     } else throw new ArrayIndexOutOfBoundsException();
   }
 
+  @Override
+  public void populateMatrix(int row, int col, E input) throws ArrayIndexOutOfBoundsException{
+    if (row < this.numOfRows && col < this.numOfCols &&
+        row >= 0 && col >= 0) {
+      content[row][col] = input;
+      content[col][row] = input;
+    } else throw new ArrayIndexOutOfBoundsException();
+  }
 }
