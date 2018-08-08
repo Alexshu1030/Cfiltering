2a06067710da9b332b41977df23aece7224eea9c
tanuanje <tanuanje@UTORONTO.CA>
2018-08-08 01:08:40 +0000

    added UMMatrixTest

---
+++ test/UserMovieMatrixTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import components.Matrix;
+import components.UserMovieMatrix;
+
+public class UserMovieMatrixTest<E> {
+  
+  private Matrix<E> matrix;
+  
+  @Before
+  public void setUp() {
+  matrix = new UserMovieMatrix<E>(3, 4);  
+  }
+  
+  @Test
+  public void populateMatrixAndGetTest() {
+    Object expected = 7;
+    matrix.populateMatrix(0, 0, (E) expected);
+    E actual = matrix.get(0, 0);
+    assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void populateMatrixOverrideAndGetTest() {
+    Object expected = 7;
+    matrix.populateMatrix(0, 0, (E) "power");
+    matrix.populateMatrix(0, 0, (E) expected);
+    E actual = matrix.get(0, 0);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void getNumRowsTest() {
+    Object expected = 3;
+    E actual = (E)(Object) matrix.getNumRows();
+    assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void getNumColsTest() {
+    Object expected = 4;
+    E actual = (E)(Object) matrix.getNumCols();
+    assertEquals(expected, actual);
+  }
+  
+
+}
