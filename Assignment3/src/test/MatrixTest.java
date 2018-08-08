9e102b94b0545ff3fe3843db32921f2d6daaaf3b
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@6c2ab6b3-a9c7-4b03-9a9e-24852ae25e8f>
2018-08-08 01:05:05 +0000

    added getNumRows/Cols tests

--- Assignment3/src/test/MatrixTest.java
+++ Assignment3/src/test/MatrixTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import components.Matrix;
+import components.UserMovieMatrix;
 
 public class MatrixTest<E> {
   
@@ -11,15 +12,39 @@ public class MatrixTest<E> {
   
   @Before
   public void setUp() {
-  matrix = new Matrix<E>(3, 3);  
+  matrix = new Matrix<E>(3, 4);  
   }
   
   @Test
-  public void populateMatrixTest() {
+  public void populateMatrixAndGetTest() {
     Object expected = 7;
     matrix.populateMatrix(0, 0, (E) expected);
     E actual = matrix.get(0, 0);
     assertEquals(expected, actual);
   }
+  
+  @Test
+  public void populateMatrixOverrideAndGetTest() {
+    Object expected = 7;
+    matrix.populateMatrix(0, 0, (E) "power");
+    matrix.populateMatrix(0, 0, (E) expected);
+    E actual = matrix.get(0, 0);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void getNumRowsTest() {
+    Object expected = 3;
+    E actual = (E)(Object) matrix.getNumRows();
+    assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void getNumColsTest() {
+    Object expected = 4;
+    E actual = (E)(Object) matrix.getNumCols();
+    assertEquals(expected, actual);
+  }
+  
 
 }
